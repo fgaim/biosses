64ed1f3cc68ab17139d45d20798d7efacdd3894d
Fitsum <fitsum.gaim@gmail.com>
2017-10-22 18:10:55 +0900

    Fix perl path by detecting OS

--- src/main/java/semanticSimilaritySystems/unsupervisedMethod/combinedOntologyMethod/UmlsSimilarity.java
+++ src/main/java/semanticSimilaritySystems/unsupervisedMethod/combinedOntologyMethod/UmlsSimilarity.java
@@ -15,57 +15,63 @@ import sun.security.util.Resources;
 public class UmlsSimilarity implements SimilarityMeasure {
 
     public double getSimilarity(String word1, String word2) throws SLIB_Exception, IOException {
-
         try {
             return calculateUmlsPairScore(word1, word2);
         } catch (InterruptedException e) {
-
             return 0;
         }
     }
 
     public static void main(String[] args) throws Exception {
-
         System.out.println(calculateUmlsPairScore("hand", "skull"));
-
     }
 
 
-     public static double calculateUmlsPairScore(String word1, String word2) throws SLIB_Exception, IOException, InterruptedException {
+    public static double calculateUmlsPairScore(String word1, String word2) throws SLIB_Exception, IOException, InterruptedException {
 
-         double similarity=0;
-         Process process = Runtime.getRuntime().exec("C:\\Strawberry\\perl\\bin\\perl.exe " + com.google.common.io.Resources.getResource("UMLS-Similarity-1.47/utils/").getFile().substring(1) +"query-umls-similarity-webinterface.pl --measure cdist --sab OMIM,MSH --rel PAR/CHD "
-         + word1 + " " + word2);
-          process.waitFor();
+        double similarity = 0;
 
-         if(process.exitValue() == 0)
-         {
-             System.out.println("Command Successful");
-         }
-         else
-         {
-             System.out.println("Command Failure");
-         }
+        String perl_path = "perl /"; // default to linux
+        if (System.getProperty("os.name", "generic")
+                .toLowerCase().indexOf("windows") > -1) {
+            perl_path = "C:\\Strawberry\\perl\\bin\\perl.exe ";
+        }
 
-         BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
-         String line;
-         while (true) {
-             line = r.readLine();
-             if (line == null) { break; }
-             System.out.println(line);
-             if(line.contains(word1) && line.contains(word2)){
-                 String[] split = line.split("<>");
-                 try {
-                     similarity=Double.parseDouble(split[0]);
-                     System.out.println(similarity);
+        String cmd = perl_path
+                + com.google.common.io.Resources.getResource("UMLS-Similarity-1.47/utils/")
+                .getFile().substring(1)
+                + "query-umls-similarity-webinterface.pl --measure cdist --sab OMIM,MSH --rel PAR/CHD "
+                + word1 + " " + word2;
+        Process process = Runtime.getRuntime().exec(cmd);
 
-                 } catch (NumberFormatException e) {
-                 }
-             }
+        process.waitFor();
 
-         }
+        if (process.exitValue() == 0) {
+            System.out.println("Command Successful");
+        } else {
+            System.out.println("Perl Failure");
+        }
 
-         System.out.println("SON:" + similarity);
-         return similarity;
+        BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) {
+                break;
+            }
+            System.out.println(line);
+            if (line.contains(word1) && line.contains(word2)) {
+                String[] split = line.split("<>");
+                try {
+                    similarity = Double.parseDouble(split[0]);
+                    System.out.println(similarity);
+                } catch (NumberFormatException e) {
+                }
+            }
+
+        }
+
+        System.out.println("SON:" + similarity);
+        return similarity;
     }
 }
